ef91972e61fc1c6f585710add72b06954ebad064
Jason Hazel <me@jasonhazel.com>
2020-07-29 16:54:44 -0400

    change require

--- lib/geopath.rb
+++ lib/geopath.rb
@@ -24,5 +24,18 @@ module Geopath
 
 end
 
+require 'geopath/client'
 
-Dir["#{File.dirname(__FILE__)}/geopath/**/*.rb"].each { |f| require(f) }
+require 'geopath/insights/inventory'
+require 'geopath/insights/segments'
+require 'geopath/insights/models/base_model'
+require 'geopath/insights/models/inventory/classification_type'
+require 'geopath/insights/models/inventory/constructions'
+require 'geopath/insights/models/inventory/illumination_type'
+require 'geopath/insights/models/inventory/media_type'
+require 'geopath/insights/models/inventory/operator'
+require 'geopath/insights/models/inventory/search'
+require 'geopath/insights/models/inventory/spot_frame_media_name'
+
+
+# Dir["#{File.dirname(__FILE__)}/geopath/**/*.rb"].each { |f| require(f) }
